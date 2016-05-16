8266efafd8132b8ab6360030433d3bbdfe0260d6
Your Name <you.example.com>
2016-05-16 19:01:43 -0400

    Submitting AprilTwoEight

---
+++ AprilTwoEight/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprilTwoEight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you prefer what is behind door 1, 2 or 3?");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                Console.WriteLine("You won a new car! ");
+                Console.ReadLine();
+            }
+            else if (userValue == "2")
+            {
+                Console.WriteLine("You won a boat! ");
+                Console.ReadLine();
+            }
+            else if (userValue == "3")
+            {
+                Console.WriteLine("You won a clunk! ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry we didn't get that. Try again");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
